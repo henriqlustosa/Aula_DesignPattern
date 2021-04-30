f4769ea38bf647840ce1ea644bbcf1fe7f1ffd9c
Henrique Lustosa <henriqlustosa@gmail.com>
2021-04-30 11:04:54 -0300

    Inicio do curso de Design Patterns - problemas de muitos ifs a classe RealizaCalculo

---
+++ DesignPatterns/CalculadorDeImpostos.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    public class CalculadorDeImpostos
+    {
+
+        public void RealizaCalculo(Orcamento orcamento, String imposto)
+        {
+
+
+            if( "ICMS".Equals(imposto))
+            {
+                double icms = orcamento.Valor* 0.1;
+                Console.WriteLine(icms);
+            }
+            else if( "ISS".Equals(imposto))
+            {
+            double iss = orcamento.Valor * 0.05;
+             Console.WriteLine(iss);
+            }
+        }
+        
+
+    }
+}
