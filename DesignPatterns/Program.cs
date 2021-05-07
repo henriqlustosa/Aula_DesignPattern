a4ac5eb23a5e44127169c7204cef6902121347ae
Henrique Lustosa <henriqlustosa@gmail.com>
2021-05-07 12:04:39 -0300

    Problema:  No dominio do imposto o calculo pode ser uma composicao desses impostos // Por exemplo: ICMS e o ISS incidem no calculo ao mesmo tempo, ou ainda o imposto ICPP pode compor o calculo.

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -68,26 +68,48 @@ namespace DesignPatterns
 
             //Console.ReadKey();
 
+            // Exemplo utilizando o dominio de uma conta do Banco
+            // Neste caso os impostos possuem uma estrutura comum: Taxação minima, Taxação máxima e uma condição de cálculo de taxação
 
-            ContaBanco conta_01 = new ContaBanco("Henrique Lustosa", "010203-00", "030201-00", 500);
-            ContaBanco conta_02 = new ContaBanco("Sabrina", "010204-00", "030201-01", 500);
-            ContaBanco conta_03 = new ContaBanco("Amariles", "010205-00", "030201-02", 500);
-            ContaBanco conta_04 = new ContaBanco("Joice Maria", "010206-00", "030201-03", 500);
+            //ContaBanco conta_01 = new ContaBanco("Henrique Lustosa", "010203-00", "030201-00", 500);
+            //ContaBanco conta_02 = new ContaBanco("Sabrina", "010204-00", "030201-01", 500);
+            //ContaBanco conta_03 = new ContaBanco("Amariles", "010205-00", "030201-02", 500);
+            //ContaBanco conta_04 = new ContaBanco("Joice Maria", "010206-00", "030201-03", 500);
 
 
-            IRelatorio relatorioSimples = new RelatorioSimples();
-            IRelatorio relatorioComplexo = new RelatorioComplexo();
+            //IRelatorio relatorioSimples = new RelatorioSimples();
+            //IRelatorio relatorioComplexo = new RelatorioComplexo();
 
-            List<ContaBanco> contas = new List<ContaBanco>();
-            contas.Add(conta_01);
-            contas.Add(conta_02);
-            contas.Add(conta_03);
-            contas.Add(conta_04);
+            //List<ContaBanco> contas = new List<ContaBanco>();
+            //contas.Add(conta_01);
+            //contas.Add(conta_02);
+            //contas.Add(conta_03);
+            //contas.Add(conta_04);
 
-            relatorioSimples.Imprime(contas);
+            //relatorioSimples.Imprime(contas);
+            //Console.ReadKey();
+            //relatorioComplexo.Imprime(contas);
+            //Console.ReadKey();
+
+            // Problema: No dominio do imposto o calculo pode ser uma composicao desses impostos
+            // Por exemplo: ICMS e o ISS incidem no calculo ao mesmo tempo, ou ainda o imposto 
+            // ICPP pode compor o calculo.
+
+            //Imposto iss = new ISScomICMS(); ?????!!!!!!  - muitas combinacoes
+            //           ou
+            //Imposto iss = new ISS(new ICMS( new ICPP));  - solucao mais elegante
+
+            IImposto iss = new ISS();
+
+            Orcamento orcamento = new Orcamento(500);
+
+            double valor = iss.Calcula(orcamento);
+            
+            Console.WriteLine(valor);
+
             Console.ReadKey();
-            relatorioComplexo.Imprime(contas);
-            Console.ReadKey();
+
+
 
 
         }
